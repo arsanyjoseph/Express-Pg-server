e6908cb3c507099006bc00733583aeceee149ca5
unknown <arsanyjoseph@gmail.com>
2023-12-20 20:48:33 +0200

    create validate fn

--- src/utils/password-handler.ts
+++ src/utils/password-handler.ts
@@ -1,4 +1,4 @@
-import { hash, genSalt } from "bcrypt";
+import { hash, genSalt, compare } from "bcrypt";
 
 const generateSalt = async (): Promise<string> => {
   return await genSalt(10);
@@ -8,10 +8,15 @@ const hashPassword = async (plainPassword: string): Promise<string> => {
   const salt = await generateSalt();
   const hashedPassword = await hash(plainPassword, salt);
   return hashedPassword;
+}
+  ;
+const validatePassword = async (plainPassword: string, hash: string): Promise<boolean> => {
+  return await compare(plainPassword, hash);
 };
 
 const passwordHandler = {
   generateSalt,
-  hashPassword
+  hashPassword,
+  validatePassword
 };
 export default passwordHandler;
