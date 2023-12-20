abd96cb0e8804d850cea2e4217709bf0fef97648
unknown <arsanyjoseph@gmail.com>
2023-12-20 20:26:20 +0200

    initialize app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
-import { Express } from 'express';
+import { type Express, type Router } from "express";
+import { type DBConnection } from "./modules/db/db";
+import { type IRoutes } from "./types/router";
 
 export class App {
-    constructor(private readonly app: Express) { }
+  constructor(
+    private readonly app: Express,
+    router: Router,
+    middlewares: any[] = [],
+    routes: IRoutes[],
+    db: DBConnection
+  ) {
+    middlewares.forEach((middleware) => {
+      this.registerMiddleware(middleware);
+    });
+    routes.forEach((route) => {
+      this.registerRouter(route.path, route.router(router, db.pool));
+    });
+    db.connect();
+  }
 
-    public listen() {
-        this.app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`))
-    }
+  private registerRouter(routePath: string, router: Router): void {
+    this.app.use(routePath, router);
+  }
+
+  private registerMiddleware(middleware: any): void {
+    this.app.use(middleware);
+  }
+
+  public listen(port?: string): void {
+    this.app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  }
 }
