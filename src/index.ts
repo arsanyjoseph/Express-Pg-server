9f78ca5532bdad409a0c1ab7f12766ff59c77ba0
unknown <arsanyjoseph@gmail.com>
2023-12-20 20:23:11 +0200

    initialize app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import express, { Express } from "express"
-import dotenv from "dotenv"
-import { App } from "./main"
+import express, { Router } from "express";
+import dotenv from "dotenv";
+import { App } from "./main";
+import { routes } from "./routes/routes";
+import { middlewares } from "./middlewares";
+import { DBConnection } from "./modules/db/db";
 
-dotenv.config()
+dotenv.config();
 
-const app = new App(express())
+const db = new DBConnection({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT ?? "5432")
+});
+const app = new App(express(), Router(), middlewares, routes, db);
 
-app.listen()
+app.listen(process.env.PORT);
