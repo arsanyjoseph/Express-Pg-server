dbdf1a34326d5075663fd9a4551ffe30830e9318
unknown <arsanyjoseph@gmail.com>
2023-12-24 14:28:23 +0200

    add deleteUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,35 +1,41 @@
-import { type Router } from "express";
+import { type Router, type NextFunction, type Response, type Request } from "express";
 import { type UserService } from "./user.service";
 import { type IRouter } from "../../types/router";
-import { type UserDto } from "./user.dto";
+import { type IError } from "../../middlewares/errorHandler.middleware";
 
 export class UserController implements IRouter {
   constructor(
     private readonly router: Router,
     private readonly userService: UserService
   ) {
-    this.router.post("/register", (req, res) => {
-      const response = this.userService.register(req.body as UserDto);
-      response
-        .then((user) => res.status(201).json(user))
-        .catch((error) => {
-          throw new Error(error as string);
-        });
+    this.router.delete("/:id", (req, res, next): void => {
+      this.deleteUser(req, res, next)
     });
-    this.router.delete("/:id", (req, res): void => {
-      const { id } = req.params;
-      this.userService
-        .deleteUser(parseInt(id))
-        .then(() => {
-          res.status(204);
-        })
-        .catch((error) => {
-          throw new Error(error as string);
-        });
-    });
+    this.router.get("/profile", (req, res, next) => { this.getProfile(req, res, next) })
   }
 
   getRouter(): Router {
     return this.router;
+  }
+
+  private getProfile(req: Request, res: Response, next: NextFunction): void {
+    res.json({ message: "auth" })
+  }
+
+  private deleteUser(req: Request, res: Response, next: NextFunction): any {
+    const { id } = req.params;
+    this.userService
+      .deleteUser(parseInt(id))
+      .then(() => {
+        res.status(204);
+      })
+      .catch((errorMessage) => {
+        const error: IError = {
+          message: errorMessage,
+          name: "Internal Error",
+          statusCode: 500
+        }
+        next(error);
+      });
   }
 }
