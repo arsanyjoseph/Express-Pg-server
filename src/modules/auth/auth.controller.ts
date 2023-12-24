089d1007ecfba0243cd1aaf034bd13848b9d456e
unknown <arsanyjoseph@gmail.com>
2023-12-24 14:24:10 +0200

    handle login & register methods

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,25 +1,53 @@
-import { type Router } from "express";
+import { type Router, type Request, type Response, type NextFunction } from "express";
 import { type AuthService } from "./auth.service";
 import { type IRouter } from "../../types/router";
 import { type AuthDto } from "./auth.dto";
+import { type IError } from "../../middlewares/errorHandler.middleware";
+import { type UserDto } from "../user/user.dto";
 
 export class AuthController implements IRouter {
   constructor(
     private readonly router: Router,
     private readonly authService: AuthService
   ) {
-    this.loginHandler();
+    this.router.post("/login", (req, res, next) => {
+      this.loginHandler(req, res, next)
+    });
+    this.router.post("/register", (req, res, next) => {
+      this.registerUser(req, res, next)
+    });
   }
 
-  private loginHandler(): void {
-    this.router.post("/login", (req, res): void => {
+  private loginHandler(req: Request, res: Response, next: NextFunction): void {
+    if (req.body?.email && req.body?.password) {
       this.authService
         .login(req.body as AuthDto)
-        .then((response) => res.status(200).json(response))
+        .then((accessToken) => res.status(200).json({ accessToken }))
         .catch((error) => {
-          throw new Error(error as string);
+          next(error);
         });
-    });
+    } else {
+      const error: IError = {
+        message: "Please provide Valid Credentials",
+        name: "Bad Request",
+        statusCode: 400
+      }
+      next(error)
+    }
+  }
+
+  private registerUser(req: Request, res: Response, next: NextFunction): void {
+    const response = this.authService.register(req.body as UserDto);
+    response
+      .then((user) => res.status(201).json(user))
+      .catch((errorMessage) => {
+        const error: IError = {
+          message: errorMessage,
+          name: "Internal Server Error",
+          statusCode: 500
+        }
+        next(error);
+      });
   }
 
   getRouter(): Router {
