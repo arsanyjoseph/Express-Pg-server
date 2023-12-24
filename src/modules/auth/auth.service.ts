38ea905bed62f544f150b8fbf141647753079b96
unknown <arsanyjoseph@gmail.com>
2023-12-24 14:24:47 +0200

    handle register

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,23 @@
-import { type UserDto } from "../user/user.dto";
 import { type AuthRepository } from "./auth.repository";
 import { type AuthDto } from "./auth.dto";
-import { removePasswordFromUserEntity } from "../../utils/removePasswordFromResponse";
-import passwordHandler from "../../utils/password-handler";
+import passwordHandler from "../../utils/passwordHandler";
+import { signToken } from "../../utils/jwt";
+import { type UserDto } from "../user/user.dto";
+import { removePasswordFromUser } from "../../utils/removePasswordFromUser";
 
 export class AuthService {
   constructor(private readonly authRepository: AuthRepository) { }
-  async login(authDto: AuthDto): Promise<Omit<UserDto, "password">> {
+
+  async login(authDto: AuthDto): Promise<string> {
     const user = await this.authRepository.login(authDto)
     const isValid = await passwordHandler.validatePassword(authDto.password, user.password)
-    if (!isValid) throw new Error("Incorrect Password")
-    return removePasswordFromUserEntity(user)
+    if (!isValid) throw new Error("Invalid Password")
+    const token = signToken({ id: user.id, email: user.email, role: user.role })
+    return token
+  }
+
+  async register(user: UserDto): Promise<Omit<UserDto, "password">> {
+    const newUser = await this.authRepository.createUser(user);
+    return removePasswordFromUser(newUser);
   }
 }
