2004f5abc124e52a411f21c98b99aa4418c419fa
unknown <arsanyjoseph@gmail.com>
2023-12-24 17:51:16 +0200

    create http constants

---
+++ src/constants/http.ts
@@ -0,0 +1,31 @@
+export const HttpStatusCode = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500,
+}
+export const HttpErrorName = {
+    BAD_REQUEST: "Bad Request",
+    UNAUTHORIZED: "UnAuthorized",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    SERVER_ERROR: "Internal Serve Error",
+}
+
+export const HttpErrorMessage = {
+    BAD_REQUEST: {
+        INVALID_CREDS: "Invalid Credentials"
+    },
+    UNAUTHORIZED: {
+        NO_TOKEN: "Please, Login",
+        USER_NOT_FOUND: "User is not found",
+        INVALID_CREDS: "Invalid Credentials"
+    },
+    SERVER_ERROR: {
+        NO_MESSAGE: "Something went wrong...",
+        DUPLICATE_CREDS: "Email is already registered. Try Signing In"
+    }
+}
