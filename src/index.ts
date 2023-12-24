f42696bc6df06bf2e5195afb1c452088ee1cb005
unknown <arsanyjoseph@gmail.com>
2023-12-24 14:29:21 +0200

    add private and public routers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express, { Router } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import { App } from "./main";
 import { routes } from "./routes/routes";
-import { middlewares } from "./middlewares";
+import { middlewares } from "./middlewares/express.middleware";
+import { authMiddleware } from "./middlewares/auth.middleware";
 import { DBConnection } from "./modules/db/db";
+import { Router } from "./routes/router";
 
 dotenv.config();
+
+const privateRouter = new Router()
+privateRouter.registerMiddleware(authMiddleware)
+const publicRouter = new Router()
 
 const db = new DBConnection({
   user: process.env.DB_USERNAME,
@@ -14,6 +20,6 @@ const db = new DBConnection({
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT ?? "5432")
 });
-const app = new App(express(), Router(), middlewares, routes, db);
+const app = new App(express(), privateRouter.getRouter(), publicRouter.getRouter(), middlewares, routes, db);
 
 app.listen(process.env.PORT);
