f23bf9d355bd0b4f3728244d977bc54cb8261afa
unknown <arsanyjoseph@gmail.com>
2023-12-24 20:05:37 +0200

    create entity

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,12 @@
+export enum UserEntity {
+    ID = "id",
+    EMAIL = "email",
+    PASSWORD = "password",
+    FIRST_NAME = "firstName",
+    LAST_NAME = "lastName",
+    CREATED_AT = "createdAt",
+    UPDATED_AT = "updatedAt",
+    DELETED_AT = "deletedAt",
+    IS_ACTIVE = "isActive",
+    ROLE = "role"
+}
