2329cbf5ae3fea6c3fe2d2963cbfc86a2d99ca67
unknown <arsanyjoseph@gmail.com>
2023-12-24 14:26:25 +0200

    add getUser method

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,16 @@
-import { removePasswordFromUserEntity } from "../../utils/removePasswordFromResponse";
+import { removePasswordFromUser } from "../../utils/removePasswordFromUser";
 import { type UserDto } from "./user.dto";
 import { type UserRepository } from "./user.repository";
 
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
-  async register(user: UserDto): Promise<Omit<UserDto, "password">> {
-    const newUser = await this.userRepository.createUser(user);
-    return removePasswordFromUserEntity(newUser);
-  }
+  constructor(private readonly userRepository: UserRepository) { }
 
   async deleteUser(id: number): Promise<void> {
     await this.userRepository.deleteUser(id);
+  }
+
+  async getUser(id: number): Promise<Omit<UserDto, "password">> {
+    const user = await this.userRepository.getUserById(id)
+    return removePasswordFromUser(user);
   }
 }
