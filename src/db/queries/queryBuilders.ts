77b3112c7b43e8ca0730373a039bd03e6e257bea
unknown <arsanyjoseph@gmail.com>
2023-12-24 20:04:51 +0200

    create interfaces for queryBuilder

--- src/db/queries/queryBuilders.ts
+++ src/db/queries/queryBuilders.ts
@@ -1,17 +1,42 @@
-export const SelectQueryBuilder = (columnName: string = "*", tableName: string, condition?: string): string => {
+interface ICell {
+    columnName: string
+    value: string
+}
+
+export interface ISelect {
+    columnName: string
+    tableName: string
+    condition?: string
+}
+
+export interface IInsert {
+    tableName: string
+    insertedColumns: ICell[]
+    returnColumns?: string[]
+}
+export interface IUpdate {
+    tableName: string
+    updatedColumns: ICell[]
+    condition?: string
+    returnColumns?: string[]
+}
+
+export const SelectQueryBuilder = ({ columnName, tableName, condition }: ISelect): string => {
     const baseQuery = `SELECT ${columnName} from public.${tableName}`
     return condition ? `${baseQuery} WHERE ${condition}` : baseQuery
 }
 
-export const UpdateQueryBuilder = (tableName: string, columnName: string, value: number | string | boolean | null, condition?: string): string => {
-    const baseQuery = `UPDATE public.${tableName} SET ${columnName} = ${value}`
-    return condition ? `${baseQuery} WHERE ${condition}` : baseQuery
+export const UpdateQueryBuilder = ({ tableName, updatedColumns, condition, returnColumns }: IUpdate): string => {
+    const baseQuery = `UPDATE public.${tableName}`
+    const update = updatedColumns.map(({ columnName, value }) => `"${columnName}" = ${value}`).join(", ")
+    const returning = returnColumns?.join(", ")
+    return `${baseQuery} SET ${update} ${condition ? 'WHERE ' + condition : ""} RETURNING ${returning}`
 }
 
-export const InsertQueryBuilder = (tableName: string, columns: string[], values: string[], returnColumns?: string[]): string => {
+export const InsertQueryBuilder = ({ tableName, returnColumns, insertedColumns }: IInsert): string => {
     const baseQuery = `INSERT INTO public.${tableName}`
-    const columnsString = `(${columns.join(", ")})`
-    const valuesString = `(${values.join(", ")})`
+    const columnsString = `(${insertedColumns.map(column => `"${column.columnName}"`).join(", ")})`
+    const valuesString = `(${insertedColumns.map(column => column.value).join(", ")})`
     const returning = returnColumns?.join(", ")
-    return `${baseQuery} ${columnsString} VALUES ${valuesString} ${returning}`;
+    return `${baseQuery} ${columnsString} VALUES ${valuesString} RETURNING ${returning}`;
 }
