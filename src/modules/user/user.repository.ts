6dce06be11ecb2e8a258cff78232ec9929e46d0b
unknown <arsanyjoseph@gmail.com>
2023-12-26 21:56:59 +0200

    move logic to PoolWrapper

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,59 +1,37 @@
-import { type Pool } from "pg";
 import { type UserDto } from "./user.dto";
 import { HttpErrorMessage } from "../../constants/http";
-import { type ISelect, SelectQueryBuilder, UpdateQueryBuilder, type IUpdate } from "../../db/queries/queryBuilders";
+import { type PoolWrapper } from "../../db/PoolWrapper/PoolWrapper";
 import { UserEntity } from "./user.entity";
 
 export class UserRepository {
-  constructor(private readonly pool: Pool) { }
+  constructor(private readonly poolWrapper: PoolWrapper) { }
 
   async getUserByEmail(email: string): Promise<UserDto> {
-    const queryParams: ISelect = { columnName: "*", tableName: "user", condition: "email = $1" }
-    const query = {
-      text: SelectQueryBuilder(queryParams),
-      values: [email]
-    };
-    const users = (await this.pool.query<UserDto>(query)).rows;
-    if (!users) throw new Error(HttpErrorMessage.UNAUTHORIZED.USER_NOT_FOUND)
-    return users[0];
+    const user = await this.poolWrapper.findOne<UserDto>("*", { email })
+    return user;
   }
 
   async getUserById(id: number): Promise<UserDto> {
-    const queryParams: ISelect = { columnName: "*", tableName: "user", condition: "id = $1" }
-    const query = {
-      text: SelectQueryBuilder(queryParams),
-      values: [id]
-    };
-    const users = (await this.pool.query<UserDto>(query)).rows;
-    if (!users) throw new Error(HttpErrorMessage.UNAUTHORIZED.USER_NOT_FOUND)
-    return users[0];
+    const user = await this.poolWrapper.findOne<UserDto>("*", { id })
+    return user;
+  }
+
+  async createUser(userDto: UserDto): Promise<UserDto> {
+    const users = await this.poolWrapper.create<UserDto>(userDto)
+    return users[0]
   }
 
   async updateUserProfile({ id, firstName, lastName, isActive, }: UserDto): Promise<UserDto> {
     const user = await this.getUserById(id)
     if (!user) throw new Error(HttpErrorMessage.UNAUTHORIZED.USER_NOT_FOUND)
 
-    const updated = {
-      firstName: firstName ?? user.firstName,
-      lastName: lastName ?? user.lastName,
-      isActive: isActive ?? user.isActive,
+    const updatedColumns = {
+      [UserEntity.FIRST_NAME]: firstName ?? user.firstName,
+      [UserEntity.LAST_NAME]: lastName ?? user.lastName,
+      [UserEntity.IS_ACTIVE]: isActive ?? user.isActive,
+      [UserEntity.UPDATED_AT]: new Date().toISOString()
     }
-    const queryParams: IUpdate = {
-      tableName: "user",
-      updatedColumns: [
-        { columnName: UserEntity.FIRST_NAME, value: "$1" },
-        { columnName: UserEntity.LAST_NAME, value: "$2" },
-        { columnName: UserEntity.IS_ACTIVE, value: "$3" },
-        { columnName: UserEntity.UPDATED_AT, value: "$4" }],
-      condition: `${UserEntity.ID} = $5`,
-      returnColumns: ["*"]
-    }
-
-    const query = {
-      text: UpdateQueryBuilder(queryParams),
-      values: [updated.firstName, updated.lastName, updated.isActive, new Date().toISOString(), id]
-    };
-    const users = (await this.pool.query<UserDto>(query)).rows;
+    const users = await this.poolWrapper.updateEntity<UserDto>(updatedColumns, { id })
     if (!users) throw new Error(HttpErrorMessage.UNAUTHORIZED.USER_NOT_FOUND)
     return users[0];
   }
@@ -61,15 +39,11 @@ export class UserRepository {
   async deleteUser(id: number): Promise<void> {
     const foundUser = await this.getUserById(id);
     if (!foundUser) throw new Error(HttpErrorMessage.UNAUTHORIZED.USER_NOT_FOUND);
-    const queryParams: IUpdate = {
-      tableName: "user",
-      updatedColumns: [{ columnName: UserEntity.DELETED_AT, value: "$1" }],
-      condition: `${UserEntity.ID} = $2`,
+
+    const updatedColumn = {
+      [UserEntity.DELETED_AT]: new Date().toISOString()
     }
-    const query = {
-      text: UpdateQueryBuilder(queryParams),
-      values: [new Date().toISOString(), foundUser.id]
-    };
-    await this.pool.query(query);
+
+    await this.poolWrapper.updateEntity<UserDto>(updatedColumn, { id });
   }
 }
