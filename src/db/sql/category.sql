8d0494d4f350748273fe6a58d0c8e64303ca21c1
unknown <arsanyjoseph@gmail.com>
2024-01-01 13:48:22 +0200

    create category table

---
+++ src/db/sql/category.sql
@@ -0,0 +1,18 @@
+DO $$ BEGIN IF NOT EXISTS (
+    SELECT 1
+    FROM pg_tables
+    WHERE schemaname = 'public'
+        AND tablename = 'category'
+) THEN CREATE TABLE IF NOT EXISTS category (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP NOT NULL,
+    "deletedAt" TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL,
+    "userId" INTEGER,
+    FOREIGN KEY ("userId") REFERENCES "user"(id)
+);
+END IF;
+END $$;
