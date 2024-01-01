09def151bc135a9b73384f38d9118bfed882bf9a
unknown <arsanyjoseph@gmail.com>
2024-01-01 13:48:57 +0200

    add category.sql to initial queries

--- src/constants/initialQueries.ts
+++ src/constants/initialQueries.ts
@@ -2,5 +2,6 @@ export const initialQueries = {
     database_smart_quiz: "db.sql",
     user_roles_type_enum: "roles.sql",
     user_table: "user.sql",
+    category_table: "category.sql",
     quiz_table: "quiz.sql",
 }
