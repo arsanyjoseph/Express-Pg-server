8d370e0e67664ce9df8cf259e161ac8de322312a
unknown <arsanyjoseph@gmail.com>
2024-01-01 13:52:04 +0200

    add category module to registered modules

--- src/utils/registerModules.ts
+++ src/utils/registerModules.ts
@@ -2,7 +2,8 @@ import type { Module, } from "../types/router";
 import { AuthModule } from "../modules/auth/auth.module";
 import { UserModule } from "../modules/user/user.module";
 import type { App } from "../main";
+import { CateogryModule } from "../modules/category/category.module";
 
 
-export const registerModules = (app: App): Module[] => [new AuthModule(app), new UserModule(app)]
+export const registerModules = (app: App): Module[] => [new AuthModule(app), new UserModule(app), new CateogryModule(app)]
 
