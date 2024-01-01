4f095dfd42debf76e6e68a85757cfdca7b7282f0
unknown <arsanyjoseph@gmail.com>
2024-01-01 13:51:25 +0200

    create category repo

---
+++ src/modules/category/category.repository.ts
@@ -0,0 +1,56 @@
+import { HttpErrorMessage } from "../../constants/httpResponse";
+import { type PoolWrapper } from "../../db/PoolWrapper/PoolWrapper";
+import { type CategoryDto } from "./category.dto";
+import { CategoryEntity } from "./category.entity";
+
+export class CategoryRepository {
+    constructor(private readonly poolWrapper: PoolWrapper) { }
+
+    async getCategoryById(id: number): Promise<CategoryDto> {
+        const category = await this.poolWrapper.findOne<CategoryDto>("*", { id })
+        return category
+    }
+
+    async getCategoryByName(name: string): Promise<CategoryDto> {
+        const category = await this.poolWrapper.findOne<CategoryDto>("*", { name })
+        return category
+    }
+
+    async createCategory(categoryDto: CategoryDto): Promise<CategoryDto> {
+        const category: CategoryDto = {
+            ...categoryDto,
+            [CategoryEntity.CREATED_AT]: new Date().toISOString(),
+            [CategoryEntity.UPDATED_AT]: new Date().toISOString(),
+            [CategoryEntity.IS_ACTIVE]: true,
+            [CategoryEntity.DELETED_AT]: null,
+        }
+        const newCategories = await this.poolWrapper.create<CategoryDto>(category)
+        return newCategories[0]
+    }
+
+    async updateCategory(categoryDto: CategoryDto): Promise<CategoryDto> {
+        const { id, name, description, isActive } = categoryDto
+        const foundCategory = await this.getCategoryById(categoryDto.id)
+        if (!foundCategory) throw new Error(HttpErrorMessage.NOT_FOUND.NO_RECORD);
+
+        const updatedColumns = {
+            [CategoryEntity.NAME]: name ?? foundCategory.name,
+            [CategoryEntity.DESCRIPTION]: description ?? foundCategory.description,
+            [CategoryEntity.IS_ACTIVE]: isActive ?? foundCategory.isActive,
+            [CategoryEntity.UPDATED_AT]: new Date().toISOString()
+        }
+        const newCategories = await this.poolWrapper.updateEntity<CategoryDto>(updatedColumns, { id })
+        return newCategories[0]
+    }
+
+    async deleteCategory(id: number): Promise<void> {
+        const foundCategory = await this.getCategoryById(id);
+        if (!foundCategory) throw new Error(HttpErrorMessage.NOT_FOUND.NO_RECORD);
+
+        const updatedColumn = {
+            [CategoryEntity.DELETED_AT]: new Date().toISOString()
+        }
+
+        await this.poolWrapper.updateEntity<CategoryDto>(updatedColumn, { id });
+    }
+}
