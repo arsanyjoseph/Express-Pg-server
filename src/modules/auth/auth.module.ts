22184ad4071c17d0b61189e51b4e9f95bdcb877c
unknown <arsanyjoseph@gmail.com>
2024-01-01 10:46:28 +0200

    modules accepts app in their constructor

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,29 +1,32 @@
 import { type Router } from "express";
-import { type Pool } from "pg";
 import { PoolWrapper } from "../../db/PoolWrapper/PoolWrapper";
-import type { IRouters, Module } from "../../types/router";
+import type { Module } from "../../types/router";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { AuthRepository } from "./auth.repository";
 import { UserRepository } from "../user/user.repository";
 import { ValidatorMiddleware } from "../../middlewares/validator.middleware";
+import type { App } from "../../main";
 
 export class AuthModule implements Module {
     readonly path: string = "/auth"
     private readonly tableName: string = "user"
     readonly isPrivate: boolean = false
-    readonly authController: AuthController
-    constructor({ privateRouter, publicRouter }: IRouters, pool: Pool) {
-        const router = this.isPrivate ? privateRouter : publicRouter
-        this.authController = new AuthController(router,
-            new AuthService(
-                new AuthRepository
-                    (new UserRepository(
-                        new PoolWrapper(this.tableName, pool)))),
-            new ValidatorMiddleware())
+    private readonly router: Router
+    private readonly poolWrapper: PoolWrapper
+    private readonly authRepository: AuthRepository
+    private readonly authService: AuthService
+    private readonly authController: AuthController
+    constructor(app: App) {
+        this.router = this.isPrivate ? app.getRouters().privateRouter : app.getRouters().publicRouter
+        this.poolWrapper = new PoolWrapper(this.tableName, app.getPool())
+        this.authRepository = new AuthRepository(new UserRepository(this.poolWrapper))
+        this.authService = new AuthService(this.authRepository)
+        this.authController = new AuthController(this.router, this.authService, new ValidatorMiddleware())
+        app.registerRouter(this.path, this.getRouter())
     }
 
-    getRouter(): Router {
+    private getRouter(): Router {
         return this.authController.getRouter()
     }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,22 +1,30 @@
 import { type Router } from "express";
-import { type Pool } from "pg";
 import { UserRepository } from "./user.repository";
 import { PoolWrapper } from "../../db/PoolWrapper/PoolWrapper";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
-import type { IRouters, Module } from "../../types/router";
+import type { Module } from "../../types/router";
+import type { App } from "../../main";
 
 export class UserModule implements Module {
     readonly path: string = "/user"
-    readonly isPrivate: boolean = true
     private readonly tableName: string = "user"
+    readonly isPrivate: boolean = true
+    private readonly router: Router
+    private readonly poolWrapper: PoolWrapper
+    private readonly userRepository: UserRepository
+    private readonly userService: UserService
     private readonly userController: UserController
-    constructor({ privateRouter, publicRouter }: IRouters, pool: Pool) {
-        const router = this.isPrivate ? privateRouter : publicRouter
-        this.userController = new UserController(router, new UserService(new UserRepository(new PoolWrapper(this.tableName, pool))))
+    constructor(app: App) {
+        this.router = this.isPrivate ? app.getRouters().privateRouter : app.getRouters().publicRouter
+        this.poolWrapper = new PoolWrapper(this.tableName, app.getPool())
+        this.userRepository = new UserRepository(this.poolWrapper)
+        this.userService = new UserService(this.userRepository)
+        this.userController = new UserController(this.router, this.userService)
+        app.registerRouter(this.path, this.getRouter())
     }
 
-    getRouter(): Router {
+    private getRouter(): Router {
         return this.userController.getRouter()
     }
 }
